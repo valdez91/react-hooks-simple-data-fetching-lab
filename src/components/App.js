950aef208db8701777ae0aa3df42b425010d17ec
Victor Kemboi <vickemtoo97@gmail.com>
2024-05-02 17:29:19 +0300

    react fetching

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
-// create your App component here
+
+import React, { useState, useEffect } from "react";
+
+
+const BaseUrl='https://dog.ceo/api/breeds/image/random';
+const App=()=> {
+  const [Pic, setPic] = useState(null);
+
+  useEffect(() => {
+    fetch(BaseUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setPic(data.message);
+      });
+  }, []);
+
+  if (!Pic) return <p>Loading...</p>;
+
+  return <img src={Pic} alt="A Random Dog" />;
+}
+
+export default App;
+
